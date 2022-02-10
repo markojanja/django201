c9d10f6de6459de71270c8e753e1dd74753d24f3
Marko Janjic <marko.m.janjic@gmail.com>
2022-02-10 18:57:54 +0100

    Update settings.py

--- til/settings.py
+++ til/settings.py
@@ -22,7 +22,7 @@ print(PROJECT_DIR)
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'someawesomekey'
+#SECRET_KEY = 'someawesomekey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
